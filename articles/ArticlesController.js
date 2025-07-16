7085b29c00da46dffff4582b0561d659d7e7d4c8
Eleazar Ribeiro <eliribeiro2010@gmail.com>
2025-07-16 16:19:57 -0300

    Adicionando categorias na edição de artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
-// const Category = require("./Category");
+const Category = require("../categories/Category");
 // const slugify = require("slugify");
 
 router.get("/admin/articles/new", (req, res) => {
-    res.send("Rota para criar um novo Artigo");
-    // res.render("admin/categories/new");
+    // res.send("Rota para criar um novo Artigo");
+    Category.findAll().then(categories => {
+        res.render("admin/articles/new", {categories: categories});
+    })
 });
-
-
 
 module.exports = router;
 
