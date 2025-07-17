701ec2f829658c5380e59bf7cfd8cbb501615103
Eleazar Ribeiro <eliribeiro2010@gmail.com>
2025-07-17 10:30:24 -0300

    Adicionando o artigo no BD

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,13 +1,34 @@
 const express = require("express");
 const router = express.Router();
 const Category = require("../categories/Category");
-// const slugify = require("slugify");
+const Article = require("./Article");
+const slugify = require("slugify");
+
+router.get("/admin/articles", (req, res) => {
+    res.send("Rota de artigos")
+});
 
 router.get("/admin/articles/new", (req, res) => {
     // res.send("Rota para criar um novo Artigo");
     Category.findAll().then(categories => {
         res.render("admin/articles/new", {categories: categories});
     })
+});
+
+//Salvando o artigo no bd
+router.post("/articles/save", (req, res) => {
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category //chave estrangeira gerado no bd
+    }).then(() => {
+        res.redirect("/admin/articles");
+    });
 });
 
 module.exports = router;
