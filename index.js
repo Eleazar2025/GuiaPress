f095b1695e4c781fa6c4b016aec22cbe3f232207
Eleazar Ribeiro <eliribeiro2010@gmail.com>
2025-07-18 10:00:46 -0300

    linkado o botão ler artigo com a página do artigo correspondente

--- index.js
+++ index.js
@@ -41,6 +41,25 @@ app.get("/", (req, res) => {
     });
 })
 
+app.get("/:slug",(req, res) => {
+    var slug = req.params.slug;
+    Article.findOne({
+        where: {
+            slug: slug
+        }
+    }).then(article => {
+        if(article != undefined){
+            Category.findAll().then(categories => {
+                res.render("article", {article: article, categories: categories});
+            });
+        }else{
+            res.redirect("/");
+        }
+    }).catch( err => {
+        res.redirect("/");
+    });
+})
+
 app.listen(8080, () => {
     console.log("Server at 8080");
 })
