9e5d2c596cce909d646f73e3b39e0ae90bed3b11
Eleazar Ribeiro <eliribeiro2010@gmail.com>
2025-07-18 09:47:09 -0300

    Rodando a página inicial do blog com a lista de assuntos

--- index.js
+++ index.js
@@ -36,7 +36,9 @@ app.use("/",articlesController);
 
 
 app.get("/", (req, res) => {
-    res.render("index");
+    Article.findAll().then(articles => {
+        res.render("index", {articles: articles});
+    });
 })
 
 app.listen(8080, () => {
